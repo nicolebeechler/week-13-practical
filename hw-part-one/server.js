99bed232039b7e72beca2ea4b8c984b481682266
Nicole Beechler <nmbeechler@gmail.com>
2023-12-20 21:39:22 -0500

    initial setup

---
+++ hw-part-one/server.js
@@ -0,0 +1,6 @@
+const express = require('express') // imported express from node modules
+const app = express()
+
+app.listen(3000, () => {
+    console.log("I'm Ready")
+})
---
+++ pass-it-around/server.js
@@ -0,0 +1,8 @@
+const express = require('express') // imported express from node modules
+const app = express()
+
+
+
+app.listen(3000, () => {
+    console.log("I'm Ready")
+})
