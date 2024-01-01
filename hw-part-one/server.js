cfb3d3739bede2d2d9baef4d64c2ee11afbaec26
Nicole Beechler <nmbeechler@gmail.com>
2024-01-01 17:55:43 -0500

    Magic 8 Ball express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -26,6 +26,31 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 // http://localhost:3000/tip/:total/:tipPercentage ( 
 //    http://localhost:3000/tip/100/20 => Display: 20 )
 
+// MAGIC 8 BALL
+
+app.get('/magic/:question', (req, res) => {
+
+    const answers = [
+        "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+        "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+        "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+        "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+        "Don't count on it", "My reply is no", "My sources say no",
+        "Outlook not so good", "Very doubtful"
+    ]
+    
+    const question = req.params.question.replace(/%20/g, ' ');
+    const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+    res.send(`
+        <h2>Your Question: ${question}</h2>
+        <h1>Magic 8 Ball says: ${randomAnswer}</h1>
+    `)
+})
+
+// http://localhost:3000/magic/:question
+// http://localhost:3000/magic/Will%20I%20Be%20A%20Millionaire ( Display: Question & Random Answer )
+
 app.listen(3000, () => {
     console.log("I'm Ready")
 })
